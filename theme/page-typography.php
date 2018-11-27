afbdc42437f084e02fbbd36831c58c61bfee448a
Ace <ace@mdev.digital>
2018-11-27 10:53:35 -0500

    Added most of the markdown for typography page

--- theme/page-typography.php
+++ theme/page-typography.php
@@ -1,7 +1,45 @@
 <?php get_header(); ?>
 
-<div>
-  <h1>hello worwwwld!</h1>
+<div class="glen-typography-page">
+  
+  <!-- Font Styles -->
+  <span class="glen-typography-title">Arbor Glen Typography</span>
+  <div class="glen-typography-standard">
+    <h1>h1 looks like this!</h1>
+    <h2>h2 looks like this!</h2>
+    <h3>h3 looks like this!</h3>
+    <h4>h4 looks like this!</h4>
+    <h5>h5 looks like this!</h5>
+    <h6>h6 looks like this!</h6>
+    <p class="--body">Body copy looks like this!</p>
+    <p class="--body-sidebar">Sidebar body copy looks like this!</p>
+    <p class="--body-faq">FAQ questions looks like this!</p>
+    <p class="--edu-name">Educator names looks like this!</p>
+    <p class="--edu-title">Educator title looks like this!</p>
+    <p class="--edu-quote">Educator quotes look like this!</p>
+    <p class="--rev-name">Review names looks like this!</p>
+    <a class="glen-btn">Educator title looks like this!</a>
+  </div>
+
+  <!-- Colours -->
+  <span class="glen-typography-title">Arbor Glen Colours</span>
+  <div class="glen-typography-colours">
+    <div class="glen-color-orange"></div>
+    <div class="glen-color-green"></div>
+    <div class="glen-color-blue"></div>
+    <div class="glen-color-red"></div>
+    <div class="glen-color-yellow"></div>
+    <div class="glen-color-purple"></div>
+    <div class="glen-color-darkgrey"></div>
+    <div class="glen-color-grey"></div>
+  </div>
+
+  <!-- UI Elements -->
+  <span class="glen-typography-title">Arbor Glen UL Elements</span>
+  <div class="glen-typography-elements">
+    <span>todo</span>
+  </div>
+  
   <img src="<?php bloginfo('template_url'); ?>/img/favicon.png" />
 </div>
 
