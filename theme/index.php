ab8a27b1e4dbcf36f338cb7dbba4dde47157bb80
Ace <ace@mdev.digital>
2018-11-28 15:57:58 -0500

    Home text area heavily implemented

--- theme/index.php
+++ theme/index.php
@@ -3,10 +3,10 @@
 <div class="glen-home-page">
   <div class="glen-home-hero-image">
     <div class="glen-home-hero-content-box">
-      <h1 class="u-white">Arbour Glen<br>Day Nursery</h1>
+      <!--<h1 class="u-white">Arbour Glen<br>Day Nursery</h1>-->
       <div class="glen-squiggle --yellow"></div>
-      <a class= "glen-btn --solid" href="#" title="todo">join our waiting list</a>
-      <a class= "glen-btn" href="#" title="todo">call us: 519.439.3701</a>
+      <a class= "glen-btn --solid-wg" href="#" title="todo">join our waiting list</a>
+      <a class= "glen-btn --outlined-wg" href="#" title="todo">call us: 519.439.3701</a>
       <div class="glenbox-green"></div>
     </div>
   </div>
