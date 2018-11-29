bafa8a97359fecb5583343580b00f6b4a18367dc
Ace <ace@mdev.digital>
2018-11-29 11:49:49 -0500

    Lunch commit

--- theme/page-welcome.php
+++ theme/page-welcome.php
@@ -4,7 +4,22 @@
   <div class="glen-welcome-hero"></div>
   <div class="glen-welcome-sticker"></div>
   <div class="glen-welcome-balcony"></div>
-  <div class="glen-welcome-prefooter"></div>
+  <div class="glen-welcome-prefooter">
+    <div class="glen-prefooter">
+      <h3>More About Us:</h3>
+      <div class="glen-prefooter-boxes">
+        <div class="glen-prefooter-box">
+          <span class="glen-prefooter-box-title">Our Team</span>
+        </div>
+        <div class="glen-prefooter-box">
+          <span class="glen-prefooter-box-title">Our Menus</span>
+        </div>
+        <div class="glen-prefooter-box">
+          <span class="glen-prefooter-box-title">FAQ</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
 <?php get_footer(); ?>
