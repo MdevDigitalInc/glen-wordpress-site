f16813812c30b2bac3b0c4be68870b52a72d7588
Ace <ace@mdev.digital>
2018-11-29 10:28:01 -0500

    Added markdown for mgmt roster

--- theme/page-our-team.php
+++ theme/page-our-team.php
@@ -4,7 +4,35 @@
   <div class="glen-team-hero">hero todo</div>
   <div class="glen-team-balcony">balcony todo</div>
   <div class="glen-mgmt-roster">
-    <div class="glen-roster">roster head</div>
+    <div class="glen-roster">
+      <div class="glen-roster-head">
+        <h2>Our Management Team</h2>
+        <p>Our management team is here for you any time of day. Call them or drop by the administrative offices located on our third floor, but you may often find them in our classrooms as well.</p>
+      </div>
+      <div class="glen-roster-cards">
+        <div class="glen-roster-card">
+          <div class="glen-roster-card-image"></div>
+          <span class="--edu-name">Jamie, RECE</span>
+          <p class="--edu-title">Executive Director</p>
+          <p class="--edu-bio">Jamie is a former recipient of the Children’s Service Advocacy Award from the Assoc. of Early Childhood Educators. She has been an ECE since 1979 and with Arbour Glen since 1999.</p>
+          <p class="--edu-quote">"Nothing warms my heart more than to see a joyful child fully absorbed in what they are doing.  I love caring for our team of Educators who dedicate themselves fully to children. This is my life’s passion.”</p>
+        </div>
+        <div class="glen-roster-card">
+          <div class="glen-roster-card-image"></div>
+          <span class="--edu-name u-green">Jamie, RECE</span>
+          <p class="--edu-title">Executive Director</p>
+          <p class="--edu-bio">Jamie is a former recipient of the Children’s Service Advocacy Award from the Assoc. of Early Childhood Educators. She has been an ECE since 1979 and with Arbour Glen since 1999.</p>
+          <p class="--edu-quote">"Nothing warms my heart more than to see a joyful child fully absorbed in what they are doing.  I love caring for our team of Educators who dedicate themselves fully to children. This is my life’s passion.”</p>
+        </div>
+        <div class="glen-roster-card">
+          <div class="glen-roster-card-image"></div>
+          <span class="--edu-name u-blue">Jamie, RECE</span>
+          <p class="--edu-title">Executive Director</p>
+          <p class="--edu-bio">Jamie is a former recipient of the Children’s Service Advocacy Award from the Assoc. of Early Childhood Educators. She has been an ECE since 1979 and with Arbour Glen since 1999.</p>
+          <p class="--edu-quote">"Nothing warms my heart more than to see a joyful child fully absorbed in what they are doing.  I love caring for our team of Educators who dedicate themselves fully to children. This is my life’s passion.”</p>
+        </div>
+      </div>
+    </div>
     <div>roster </div>
   </div>
   <div class="glen-team-roster">team roster todo</div>
