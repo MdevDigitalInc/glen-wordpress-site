f28f1b75fd543ef32a4a677505547749c1711d5d
Ace <ace@mdev.digital>
2018-11-29 14:36:47 -0500

    Scaffolding for Vision page plus prefooter done

--- theme/page-our-vision.php
+++ theme/page-our-vision.php
@@ -1,8 +1,27 @@
 <?php get_header(); ?>
 
-<div>
-  <h1>Our Vision todo</h1>
-  <img src="<?php bloginfo('template_url'); ?>/img/favicon.png" />
+<div class="glen-vision-page">
+  <div class="glen-vision-hero">glen-vision-hero todo</div>
+  <div class="glen-vision-mission">glen-vision-mission todo</div>
+  <div class="glen-vision-balcony">glen-vision-balcony todo</div>
+  <div class="glen-vision-bigimage">glen-vision-bigimage todo</div>
+  <div class="glen-vision-featured">glen-vision-featured todo</div>
+  <div class="glen-vision-prefooter">
+    <div class="glen-prefooter">
+      <h3>More About Us:</h3>
+      <div class="glen-prefooter-boxes">
+        <a class="glen-prefooter-box --infants" href="/infants-program" title="Our Infants Program">
+          <span class="glen-prefooter-box-title">Infants</span>
+        </a>
+        <a class="glen-prefooter-box --toddlers" href="/toddlers-program" title="Our Toddlers Program">
+          <span class="glen-prefooter-box-title">Toddlers</span>
+        </a>
+        <a class="glen-prefooter-box --preschoolers" href="/preschoolers-program" title="Our Preschoolers Program">
+          <span class="glen-prefooter-box-title">Preschoolers</span>
+        </a>
+      </div>
+    </div>
+  </div>
 </div>
 
 <?php get_footer(); ?>
