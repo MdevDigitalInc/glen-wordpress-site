63b30a6ad0f08944e42ec92ca680b07aec2d9fc0
Ace <ace@mdev.digital>
2018-11-29 10:16:40 -0500

    Beginning work on roster module

--- theme/page-our-team.php
+++ theme/page-our-team.php
@@ -3,8 +3,11 @@
 <div>
   <div class="glen-team-hero">hero todo</div>
   <div class="glen-team-balcony">balcony todo</div>
-  <div class="glen-team-roster --mgmt">mgmt roster todo</div>
-  <div class="glen-team-roster --team">team roster todo</div>
+  <div class="glen-mgmt-roster">
+    <div class="glen-roster">roster head</div>
+    <div>roster </div>
+  </div>
+  <div class="glen-team-roster">team roster todo</div>
   <div class="glen-team-prefooter">prefooter todo</div>
 </div>
 
