566c4a23b1896f5830b330e11c56469e2dea29a6
Ace <ace@mdev.digital>
2018-11-29 14:26:33 -0500

    Added href and title to prefooter links

--- theme/page-welcome.php
+++ theme/page-welcome.php
@@ -1,20 +1,20 @@
 <?php get_header(); ?>
 
 <div class="glen-welcome-page">
-  <div class="glen-welcome-hero"></div>
-  <div class="glen-welcome-sticker"></div>
-  <div class="glen-welcome-balcony"></div>
+  <div class="glen-welcome-hero">glen-welcome-hero todo</div>
+  <div class="glen-welcome-sticker">glen-welcome-sticker todo</div>
+  <div class="glen-welcome-balcony">glen-welcome-balcony todo</div>
   <div class="glen-welcome-prefooter">
     <div class="glen-prefooter">
       <h3>More About Us:</h3>
       <div class="glen-prefooter-boxes">
-        <a class="glen-prefooter-box --our-team">
+        <a class="glen-prefooter-box --our-team" href="/our-team" title="Meet our team">
           <span class="glen-prefooter-box-title">Our Team</span>
         </a>
-        <a class="glen-prefooter-box --our-menus">
+        <a class="glen-prefooter-box --our-menus" href="/our-menu" title="View our menu">
           <span class="glen-prefooter-box-title">Our Menus</span>
         </a>
-        <a class="glen-prefooter-box --faq">
+        <a class="glen-prefooter-box --faq" href="/faq" title="Read our FAQs">
           <span class="glen-prefooter-box-title">FAQ</span>
         </a>
       </div>
