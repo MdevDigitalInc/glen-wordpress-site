1e9ecf8e3d532b5a80f719599237689ba5b169b6
Ace <ace@mdev.digital>
2018-11-29 11:11:08 -0500

    Finished roster, fully responsive

--- theme/page-our-team.php
+++ theme/page-our-team.php
@@ -11,25 +11,25 @@
       </div>
       <div class="glen-roster-cards">
         <div class="glen-roster-card">
-          <div class="glen-roster-card-image"></div>
+          <div class="glen-roster-card-image --purple"></div>
           <span class="--edu-name">Jamie, RECE</span>
           <p class="--edu-title">Executive Director</p>
           <p class="--edu-bio">Jamie is a former recipient of the Children’s Service Advocacy Award from the Assoc. of Early Childhood Educators. She has been an ECE since 1979 and with Arbour Glen since 1999.</p>
           <p class="--edu-quote">"Nothing warms my heart more than to see a joyful child fully absorbed in what they are doing.  I love caring for our team of Educators who dedicate themselves fully to children. This is my life’s passion.”</p>
         </div>
         <div class="glen-roster-card">
-          <div class="glen-roster-card-image"></div>
-          <span class="--edu-name u-green">Jamie, RECE</span>
-          <p class="--edu-title">Executive Director</p>
-          <p class="--edu-bio">Jamie is a former recipient of the Children’s Service Advocacy Award from the Assoc. of Early Childhood Educators. She has been an ECE since 1979 and with Arbour Glen since 1999.</p>
-          <p class="--edu-quote">"Nothing warms my heart more than to see a joyful child fully absorbed in what they are doing.  I love caring for our team of Educators who dedicate themselves fully to children. This is my life’s passion.”</p>
+          <div class="glen-roster-card-image --green"></div>
+          <span class="--edu-name u-green">Charlene, RECE</span>
+          <p class="--edu-title">Program Supervisor</p>
+          <p class="--edu-bio">Charlene was formerly awarded Early Childhood Educator of the Year for Ontario and was also a runner up for the Prime Minister's Award for Excellence in Early Childhood Education. She has been an ECE since 1990 and with Arbour Glen since 1990.</p>
+          <p class="--edu-quote">"Sharing literature with children has always been my favourite thing to do! Whether through puppet shows, picture books or story telling, I love watching the expressions of wonder on their faces."</p>
         </div>
         <div class="glen-roster-card">
-          <div class="glen-roster-card-image"></div>
-          <span class="--edu-name u-blue">Jamie, RECE</span>
-          <p class="--edu-title">Executive Director</p>
-          <p class="--edu-bio">Jamie is a former recipient of the Children’s Service Advocacy Award from the Assoc. of Early Childhood Educators. She has been an ECE since 1979 and with Arbour Glen since 1999.</p>
-          <p class="--edu-quote">"Nothing warms my heart more than to see a joyful child fully absorbed in what they are doing.  I love caring for our team of Educators who dedicate themselves fully to children. This is my life’s passion.”</p>
+          <div class="glen-roster-card-image --blue"></div>
+          <span class="--edu-name u-blue">Cyndee, RECE</span>
+          <p class="--edu-title">Program Administrator</p>
+          <p class="--edu-bio">Cyndee has been an ECE since 1986 and with Arbour Glen since 1986.</p>
+          <p class="--edu-quote">"All four of my children went here too: Arbour Glen is very much a second family to me. I love the variety in my day, but especially working with the children – who can have a bad day when they walk into a classroom and get swarmed with hugs!”</p>
         </div>
       </div>
     </div>
