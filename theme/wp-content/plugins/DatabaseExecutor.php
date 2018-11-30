066bed9f2a99b59645c11d9a312fda5246abe8f8
Lucas Moreira <lucas@moreiradevelopment.io>
2018-11-30 11:14:16 -0500

    skdj

--- theme/wp-content/plugins/DatabaseExecutor.php
+++
@@ -1,9 +0,0 @@
-<?php 
-function databaseExecute() {
-	global $wpdb; // this is how you get access to the database
-
-  echo $_POST['query'];
-
-	wp_die(); // this is required to terminate immediately and return a proper response
-}
-?>
