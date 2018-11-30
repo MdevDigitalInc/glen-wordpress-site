7daa588859735304ec4fcfb8dd0adbd5d7bd5f31
Ace <ace@mdev.digital>
2018-11-30 15:39:55 -0500

    init

--- theme/footer.php
+++ theme/footer.php
@@ -1,3 +1,9 @@
 		<?php wp_footer(); ?>
+		<footer class="glen-footer">
+			<div class="glen-rainbow-bar"></div>
+			<div class="glen-footer-content">
+				
+			</div>
+		</footer>
 	</body>
 </html>
