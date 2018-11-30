cbc07247c3b73d3adf86f489a22d46e94fa5514a
Ace <ace@mdev.digital>
2018-11-30 14:23:09 -0500

    Added heros to all program pages

--- theme/page-toddlers-program.php
+++ theme/page-toddlers-program.php
@@ -1,7 +1,24 @@
 <?php get_header(); ?>
 
 <div class="glen-toddlers-page">
-  <div class="glen-toddlers-hero">glen-toddlers-hero todo</div>
+  <div class="glen-toddlers-hero">
+    <div class="glen-hero-image --toddlers"></div>
+    <div class="glen-hero-content --sticker">
+      <div class="glen-hero-sticker">
+        <h1>Toddlers</h1>
+        <p>Welcome Toddlers! When your little one is about 18 months, they will transition to one of our Toddler groups. Here they will have the opportunity to explore their budding independence and enjoy some exciting new adventures.</p>
+        <p>If your child was in our Infant program, we will already know a little bit about them and they will be familiar with us. Our priority is getting to know you and your child even better and developing meaningful relationships.  In our group, your little one will enjoy having a consistent yet flexible routine that gives them the security Toddlers love.</p>
+        <div class="glen-iconstrip">
+          <div class="glen-iconstrip-bar"></div>
+          <img class="glen-iconstrip-image" src="<?php bloginfo('template_url'); ?>/img/icons/icon-letter-blocks.svg" alt="Image of lettered blocks" />
+          <div class="glen-iconstrip-bar"></div>
+        </div>
+        <p>Our Educators observe the Toddlers to provide an environment that reflects their interests and supports their ongoing development in all areas. This is also a time when they further increase their social and emotional well-being through discovery and enquiry, playing alongside one another and starting to share and take turns. Toddlers are also about movement. Our indoor environment has room for large-motor play, but nothing compares to our wonderful outdoor space that provides further challenges. Your child will run, play, take new risks and experience new adventures. Our outdoor summer program invites them to dig in the sand beneath our shady sugar maple, splash in water play activities and have fun on the riding toys.  And of course, hugs and cuddles never go out of style!</p>
+        <p>Our Educators will be there each morning to welcome you and your child and to hear about how their night was. We believe the connection with families is very important to a child’s development. We take lots of photos that document babies busy at play and use ‘Seesaw’, a digital online portfolio app, to share with you a glimpse into their time spent with us! You will receive occasional snapshots of something special they are doing, and an online portfolio of your Toddler’s ‘amazing moments’ will be compiled for you. Participation in ‘Seesaw’ is optional, so we will create paper portfolios for those who choose not to be involved. We also post ‘Learning Stories’ that share a little of what we’ve been up to, so don’t forget to check our bulletin boards and other documentation. A small note will be waiting for you at pick up time, that shares about how well your child ate and slept that day.</p>
+        <a class="glen-btn --solid-pw" href="#" title="todo">join our mailing list</a>
+      </div>
+    </div>
+  </div>
   <div class="glen-toddlers-balcony">glen-toddlers-balcony todo</div>
   <div class="glen-toddlers-sticker">glen-toddlers-sticker todo</div>
   <div class="glen-toddlers-prefooter">
