4f951f747ba91334913b83208ca2e3c7b71e91de
Ace <ace@mdev.digital>
2018-11-30 16:30:49 -0500

    Updated footer with improved responsive styles

--- theme/footer.php
+++ theme/footer.php
@@ -15,7 +15,13 @@
 				<div class="glen-rainbow-bar-segment --orange"></div>
 			</div>
 			<div class="glen-footer-content">
-				<span>glen-footer-content todo</span>
+				<div class="glen-footer-info">
+					<span class="glen-footer-address">1017 Adelaide Street North, London, ON</span>
+					<span class="glen-footer-phone">519.439.3701</span>
+				</div>
+				<div class="glen-footer-copyright">
+					<span class="glen-footer-copyline">Copyright Arbour Glen Day Nursery 2018</span>
+				</div>
 			</div>
 		</footer>
 	</body>
