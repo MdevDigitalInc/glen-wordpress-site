f8158b4920ab2691a7f0df30bc1ce4ed1b2ba188
GcouttsMDev <graham@mdev.digital>
2018-12-03 14:43:11 -0500

    style changes requested in CR.

--- theme/page-faq.php
+++ theme/page-faq.php
@@ -3,18 +3,17 @@
 <div class="glen-faq-page">
   <section class="glen-faq-hero ">
     <div class="glen-hero-image --faq"></div>
-    <div class="glen-hero-content --split">
   </section>
   <section class="glen-faq-dialogue">
   
-    <div class="glen-faq-left"><!-- questions -->
+    <div class="glen-dialogue-left"><!-- questions -->
       <div class="glen-faq-desc"><!-- description -->
         <h1>FAQ</h1>
         <p>
-        We're so thrilled that you are considering Arbour Glen! Whether you've just found out you are expecting (congratulations!) or are looking for care for your 4-year old, we are here to help make your experience as effortless as possible you.
+          We're so thrilled that you are considering Arbour Glen! Whether you've just found out you are expecting (congratulations!) or are looking for care for your 4-year old, we are here to help make your experience as effortless as possible you.
         </p>
         <p>
-        We like to have a personal connection with our prospective families, so please give us a call next at 519-439-3701 to confirm your request was received, and to answer any questions you may have about our centre. Speaking of questions, let's see if we can answer some for you now!
+          We like to have a personal connection with our prospective families, so please give us a call next at 519-439-3701 to confirm your request was received, and to answer any questions you may have about our centre. Speaking of questions, let's see if we can answer some for you now!
         </p>
         <div class="glen-faq-btn">
           <a class=" glen-btn --solid-pw">DOWNLOAD OUR PARENT HANDBOOK</a>
@@ -27,24 +26,21 @@
           echo "<p class='--body-faq'>$result->Question</p>";
           echo "<p>$result->Answer</p><br/>";
         }
-        // global $post;
-        // echo $post->post_name;
-        // echo get_mdev_meta_tags($post->post_name);
         ?>
     </div><!-- /questions -->
     
-    <div class="glen-faq-column">
+    <div class="glen-dialogue-right">
       <div class="glen-faq-box"> <!-- box -->
         <h4>
-        Finding Child Care
+          Finding Child Care
         </h4>
         <div class="glen-squiggle --yellow"></div>
         <p>
-        So...where do you start when looking for childcare? A great place to begin is the London and Middlesex Child Care Waitlist. Here, you can learn about childcare centres in your area, create an account and also get on multiple wait lists quickly and easily, including with us. You can also access more information at Family Info, a web portal for parents, caregivers that links information on programs, services and resources in the London and Middlesex area for children.
+          So...where do you start when looking for childcare? A great place to begin is the London and Middlesex Child Care Waitlist. Here, you can learn about childcare centres in your area, create an account and also get on multiple wait lists quickly and easily, including with us. You can also access more information at Family Info, a web portal for parents, caregivers that links information on programs, services and resources in the London and Middlesex area for children.
         </p>
         <div class="glen-faq-btn">
         <a class="glen-btn --solid-wb" >
-        Join Our Waiting List
+          Join Our Waiting List
         </a>
         </div>
         <div class="glenbox-blue"></div>
@@ -54,11 +50,12 @@
   </section>
   <section class="glen-faq-contact">
     <h3>STILL HAVE QUETIONS?</h3> 
-    <p>Don't hesitate to get in touch any time, and we'll be pleased to help. Here are more helpful links you might want to check out.</p>
+    <p>
+      Don't hesitate to get in touch any time, and we'll be pleased to help. Here are more helpful links you might want to check out.
+    </p>
     <div class="glen-faq-btn">
       <a class="glen-btn --solid-pw" >CONTACT US</a>
     </div>
-    <div class="temp"></div>
   </section>
   <section class="glen-faq-featured">
     <div class="glen-featured">
