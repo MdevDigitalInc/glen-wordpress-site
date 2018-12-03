5bc90b012cb47d6a40f1482cc02172a1c873046b
GcouttsMDev <graham@mdev.digital>
2018-12-03 09:13:04 -0500

    committing styles.

--- theme/page-faq.php
+++ theme/page-faq.php
@@ -36,6 +36,7 @@
         <h4>
         Finding Child Care
         </h4>
+        <div class="glen-squiggle --yellow"></div>
         <p>
         So...where do you start when looking for childcare? A great place to begin is the London and Middlesex Child Care Waitlist. Here, you can learn about childcare centres in your area, create an account and also get on multiple wait lists quickly and easily, including with us. You can also access more information at Family Info, a web portal for parents, caregivers that links information on programs, services and resources in the London and Middlesex area for children.
         </p>
@@ -58,30 +59,33 @@
     <div class="temp"></div>
   </section>
   <section class="glen-faq-featured">
-    <div class="glen-faq-donate">
-      <h3>Charitable Donations</h3>
-      <p>
-        Arbour Glen is a registered charity with the Canadian Revenue Agency. Our charitable registration number is 89611 0756 RR0001, so all of our donors receive charitable receipts. Any family or individual that donates over $150.00 will have their name engraved on a plate that will be displayed on our plaque at the back entrance-way. There are several ways you can donate:
-      </p>
-    </div>
-    <div class="glen-faq-points">
-      <ul>
-        <li>
-          Send a cheque or money order to 	1017 Adelaide St. N. London ON. N5Y 2M8
-        </li>
-        <li>
-          Stop by our office to do an Interac transaction
-        </li>
-        <li>
-          Send an email transfer to centre@arbourglen.ca
-        </li>
-        <li>
-          Donate through payroll via the United Way Workplace<br/> 			Donation Program (ask your employer)
-        </li>
-        <li>
-          Donate through Canada Helps www.canadahelps.org
-        </li>
-      </ul>
+    <div class="glen-featured">
+      <div class="glen-featured-text-object">
+        <h3>Charitable Donations</h3>
+        <p>
+          Arbour Glen is a registered charity with the Canadian Revenue Agency. Our charitable registration number is 89611 0756 RR0001, so all of our donors receive charitable receipts. Any family or individual that donates over $150.00 will have their name engraved on a plate that will be displayed on our plaque at the back entrance-way. There are several ways you can donate:
+        </p>
+      </div>
+      <div class="glen-featured-glenbox-object">
+        <ul>
+          <li>
+            Send a cheque or money order to 	1017 Adelaide St. N. London ON. N5Y 2M8
+          </li>
+          <li>
+            Stop by our office to do an Interac transaction
+          </li>
+          <li>
+            Send an email transfer to centre@arbourglen.ca
+          </li>
+          <li>
+            Donate through payroll via the United Way Workplace<br/> 			Donation Program (ask your employer)
+          </li>
+          <li>
+            Donate through Canada Helps www.canadahelps.org
+          </li>
+        </ul>
+        <div class="glenbox-green"></div>
+      </div>
     </div>
   </section>
   <section class="glen-faq-bumper">
