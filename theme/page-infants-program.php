9ab2cca09b5363c991481e723c126a72485eb3f1
Ace <ace@mdev.digital>
2018-12-03 09:58:42 -0500

    Added basic sticker sections to all program pages

--- theme/page-infants-program.php
+++ theme/page-infants-program.php
@@ -18,7 +18,17 @@
       </div>
     </div>
   </div>
-  <div class="glen-infants-balcony">glen-infants-balcony todo</div>
+  <div class="glen-infants-balcony">
+    <div class="glen-sticker --programs">
+      <div class="glen-sticker-box">
+        <h2>What our families say</h2>
+        <div class="glen-squiggle --blue"></div>
+        <p>“When I picked up my little guy on one of his first days with Arbour Glen, Chris was singing him the ABC song, while he sat contentedly in her arms.  She said (happily!) that it was her tenth time singing it – she said she discovered in that moment, it was what gave him comfort.  It almost brought me to tears: knowing they will do anything to give him what he needed in that moment of sadness.  You really feel like your baby is the centre of their world – what more can a mom ask for?”</p>
+        <span class="glen-review-author">- Alina M.</span>
+        <div class="glenbox-white"></div>
+      </div>
+    </div>
+  </div>
   <div class="glen-infants-sticker">glen-infants-sticker todo</div>
   <div class="glen-infants-prefooter">
     <div class="glen-prefooter">
--- theme/page-toddlers-program.php
+++ theme/page-toddlers-program.php
@@ -20,7 +20,17 @@
     </div>
   </div>
   <div class="glen-toddlers-balcony">glen-toddlers-balcony todo</div>
-  <div class="glen-toddlers-sticker">glen-toddlers-sticker todo</div>
+  <div class="glen-toddlers-sticker">
+    <div class="glen-sticker --programs">
+      <div class="glen-sticker-box">
+        <h2>What our families say</h2>
+        <div class="glen-squiggle --blue"></div>
+        <p>“Arbour Glen is a parent’s dream. When we were able to get a spot, we were overjoyed. A clean and positive environment staffed with the most caring professionals I have ever met and managed by impeccably wonderful people. Our daughter is in one of the toddler rooms, yet every single person in the centre knows her name, and I know in my heart that everyone one who works at Arbour Glen truly cares about every infant, toddler and pre-schooler. All parents should feel reassured and worry-free as we do.”</p>
+        <span class="glen-review-author">- John Siambanopoulos</span>
+        <div class="glenbox-white"></div>
+      </div>
+    </div>
+  </div>
   <div class="glen-toddlers-prefooter">
     <div class="glen-prefooter">
       <h3>More About Us:</h3>
