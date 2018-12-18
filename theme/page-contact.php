42fb28f3f8adff61c47fcd9928cda1ac7ce7861a
Ace <ace@mdev.digital>
2018-12-18 12:54:22 -0500

    init

--- theme/page-contact.php
+++ theme/page-contact.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div class="glen-contact-page">
-  <div class="mdev-todo">conact page todo</div>
+  <div class="mdev-todo">conact page! todo</div>
 </div>
 
 <?php get_footer(); ?>
