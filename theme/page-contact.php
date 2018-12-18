67f593890fd08a522f670cde1ffeec2ca7650cff
Ace <ace@mdev.digital>
2018-12-18 16:56:59 -0500

    Added contact page scaffolding

--- theme/page-contact.php
+++ theme/page-contact.php
@@ -1,7 +1,16 @@
 <?php get_header(); ?>
 
 <div class="glen-contact-page">
-  <div class="mdev-todo">conact page! todo</div>
+  <div class="glen-contact-page-content">
+    <div class="glen-contact-page-bigimage" style="background-image: url(<?php bloginfo('template_url'); ?>/img/shared/boy-infant-toys.png);"></div>
+    <div class="glen-contact-page-sidebar">
+      <div class="glen-contact-page-info">
+        <h2>Get in touch</h2>
+        <div class="glen-squiggle --blue"></div>
+      </div>
+      <div class="glen-contact-page-map"></div>
+    </div>
+  </div>
 <!-- PHP MAGIC </div> -->
 
 <?php get_footer(); ?>
