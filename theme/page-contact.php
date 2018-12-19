87f4c822616fb89795445abb5b1b49c25291e8c9
Ace <ace@mdev.digital>
2018-12-19 10:58:07 -0500

    Mostly done contact page

--- theme/page-contact.php
+++ theme/page-contact.php
@@ -2,13 +2,21 @@
 
 <div class="glen-contact-page">
   <div class="glen-contact-page-content">
-    <div class="glen-contact-page-bigimage" style="background-image: url(<?php bloginfo('template_url'); ?>/img/shared/boy-infant-toys.png);"></div>
+    <div class="glen-contact-page-bigimage" style="background-image: url(<?php bloginfo('template_url'); ?>/img/shared/arbour-glen-building.png);"></div>
     <div class="glen-contact-page-sidebar">
       <div class="glen-contact-page-info">
         <h2>Get in touch</h2>
         <div class="glen-squiggle --blue"></div>
+        <h4>Reach us</h4>
+        <span class="glen-contact-page-info-phone">519.439.3701</span>
+        <h4>Hours</h4>
+        <span class="glen-contact-page-info-hours">7:30am - 5:30pm<br>Monday - Friday</span>
       </div>
-      <div class="glen-contact-page-map"></div>
+      <div class="glen-contact-page-map">
+        <div class="glen-contact-page-map-container">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2917.624690236664!2d-81.24312088419073!3d43.007236779148805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882eede58a86bbb7%3A0xa8f941f3024c6a73!2sArbour+Glen+Day+Nursery!5e0!3m2!1sen!2sca!4v1545232497476" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+      </div>
     </div>
   </div>
 <!-- PHP MAGIC </div> -->
