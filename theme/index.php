25a152febd15df78dbe61d48dbf17bf4f98cbfca
Ace <ace@mdev.digital>
2018-12-19 13:10:02 -0500

    Adjusted home page styles

--- theme/index.php
+++ theme/index.php
@@ -5,7 +5,7 @@
   <div class="glen-home-hero-content-box">
     <h1 class="u-white">Arbour Glen<br>Day Nursery</h1>
     <div class="glen-squiggle --yellow"></div>
-    <a class= "glen-btn --solid-wg" href="#" title="todo">join our waiting list</a><br>
+    <a class= "glen-btn --solid-wg --juicy" href="#" title="todo">join our waiting list</a><br>
     <a class= "glen-btn --outlined-wg" href="#" title="todo">call us: 519.439.3701</a>
     <div class="glenbox-green --house"></div>
   </div>
