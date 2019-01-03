2367616426c80efe972b843e39410aedc4dd75fb
Ace <ace@mdev.digital>
2019-01-03 09:28:46 -0500

    init

--- theme/page-our-menus.php
+++ theme/page-our-menus.php
@@ -33,8 +33,46 @@
 
   <!-- Menu Section -->
 
-  <section class="glen-menus-menu mdev-todo">
-    glen-menus-menu todo
+  <section class="glen-menus-menu">
+    <div class="glen-menu-container">
+      <h4>Fall/Winter Menu</h4>
+      <div class="glen-menu-weeks">
+        <div class="glen-menu-weeks-button">
+        </div>
+        <h2>Week 1</h2>
+        <div class="glen-menu-weeks-button">
+        </div>
+      </div>
+      <div class="glen-menu-days">
+        <h3>MON</h3>
+        <h3>TUES</h3>
+        <h3>WED</h3>
+        <h3>THURS</h3>
+        <h3>FRI</h3>
+      </div>
+      <div class="glen-menu-content">
+
+      </div>
+    </div>
+  </section>
+
+  <!-- Pre-footer Section -->
+
+  <section class="glen-vision-prefooter">
+    <div class="glen-prefooter">
+      <h3>Our Programs:</h3>
+      <div class="glen-prefooter-boxes">
+        <a class="glen-prefooter-box --infants" style="background-image: url(<?php bloginfo('template_url'); ?>/img/prefooter/footer-infants.png);" href="/infants-program" title="Our Infants Program">
+          <span class="glen-prefooter-box-title">Infants</span>
+        </a>
+        <a class="glen-prefooter-box --toddlers" style="background-image: url(<?php bloginfo('template_url'); ?>/img/prefooter/footer-toddlers.png);" href="/toddlers-program" title="Our Toddlers Program">
+          <span class="glen-prefooter-box-title">Toddlers</span>
+        </a>
+        <a class="glen-prefooter-box --preschoolers" style="background-image: url(<?php bloginfo('template_url'); ?>/img/prefooter/footer-preschoolers.png);" href="/preschoolers-program" title="Our Preschoolers Program">
+          <span class="glen-prefooter-box-title">Preschoolers</span>
+        </a>
+      </div>
+    </div>
   </section>
 
 </div>
