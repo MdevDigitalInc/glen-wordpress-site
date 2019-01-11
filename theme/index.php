03028b964e1174e7ec20dab409690deb4c8bd585
Ace <ace@mdev.digital>
2019-01-11 10:11:04 -0500

    lint

--- theme/index.php
+++ theme/index.php
@@ -5,7 +5,7 @@
   <div class="glen-home-hero-content-box">
     <h1 class="u-white">Arbour Glen<br>Day Nursery</h1>
     <div class="glen-squiggle --yellow"></div>
-    <a class= "glen-btn --solid-wg --juicy"  title="Join our waiting list" href="mailto:centre@arbourglen.ca">join our waiting list</a><br>
+    <a class= "glen-btn --solid-wg --juicy" title="Join our waiting list" href="mailto:centre@arbourglen.ca">join our waiting list</a><br>
     <a class= "glen-btn --outlined-wg" href="tel:5194393701" title="Give us a call!">call us: 519.439.3701</a>
     <div class="glenbox-green --house"></div>
   </div>
