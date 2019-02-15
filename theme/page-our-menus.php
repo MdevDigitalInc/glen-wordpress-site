2cf720e40bd1e5d7f6aaf8e6a046224dc100f635
Lucas Moreira <lucas@moreiradevelopment.io>
2019-02-15 10:53:31 -0500

    Updated menu opening paragraph

--- theme/page-our-menus.php
+++ theme/page-our-menus.php
@@ -15,7 +15,7 @@
     <div class="glen-hero-content">
       <div class="glen-hero-basic">
         <h1>Our Menus</h1>
-        <p>At Arbour Glen, we know that sound nutritional practices and healthy eating are essential for optimal growth and development of children and is linked to their brain development and self-esteem. Helping children make good food choices during childhood influences lifelong eating habits. Our menus reflect healthy food and drink choices for children to meet the requirements as set out in the “Eating Well with Canada’s Food Guide”. Tasty meals and snacks are prepared on-site, limiting added sugar and salt when possible.</p>
+        <p>At Arbour Glen, we know that sound nutritional practices and healthy eating are essential for optimal growth and development of children and is linked to their brain development and self-esteem. Helping children make good food choices during childhood influences lifelong eating habits. Our menus reflect healthy food and drink choices for children to meet the requirements as set out in the 2019 “Canada’s Food Guide”. Tasty meals and snacks are prepared on-site, limiting added sugar and salt when possible.</p>
       </div>
     </div>
   </section>
